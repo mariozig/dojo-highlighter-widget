2f7824b3e1b7779268acbd2670499627af66093b
robert <robert@MTVL130e7c851.local>
2016-12-27 13:03:58 -0800

    Robert

--- TextHighlighterWidget.js
+++ TextHighlighterWidget.js
@@ -1,12 +1,35 @@
 define([
-    "dojo/_base/declare"
+    "dojo/_base/declare",
+    "dojo/dom-style",
+    "dojo/dom"
     //  add imports here
-], function(declare){ // update imports here
+], function(declare, domStyle, dom){ // update imports here
     "use strict";
 
     var target;
 
     return declare(null, {
       // add methods here
+
+      init: function(inputText, targetDiv){
+        target = dom.byId(targetDiv);
+
+
+        //target = document.getElementById(targetDiv);
+        target.innerHTML = inputText;
+      },
+
+
+      highlight: function(color){
+        domStyle.set(target, "background-color", color)
+
+        //target.style.backgroundColor = color;
+      }
+
+      /*
+      init("Highlight me, bro!!", "mydiv");
+
+      highlight("yellow");
+      */
     });
 });

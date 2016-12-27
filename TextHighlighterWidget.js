a27adba75baea19b2daaca5faaa07237cc0cefa9
Mario Zigliotto <mario_zigliotto@intuit.com>
2016-12-27 10:30:07 -0800

    Remove answers

--- TextHighlighterWidget.js
+++ TextHighlighterWidget.js
@@ -1,20 +1,12 @@
 define([
-    "dojo/_base/declare",
-    "dojo/dom",
-    "dojo/dom-style"
-], function(declare, dom, domStyle){
+    "dojo/_base/declare"
+    //  add imports here
+], function(declare){ // update imports here
     "use strict";
 
     var target;
 
     return declare(null, {
-        init: function (text, targetId) {
-            target = dom.byId(targetId);
-            target.innerHTML = "<p>" + text + "</p>";
-        },
-
-        highlight: function (color) {
-            domStyle.set(target, "background-color", color);
-        }
+      // add methods here
     });
 });
